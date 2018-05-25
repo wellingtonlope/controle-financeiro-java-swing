3df510a96f69f47193b66ad0232ef5a2d12b70b0
Wellington Lopes <wellington.lopes.tw@gmail.com>
2018-05-25 15:17:23 -0400

    [ADD] interface e logica do software

---
+++ src/controle/financeiro/java/swing/Movimentacao.java
@@ -0,0 +1,24 @@
+package controle.financeiro.java.swing;
+
+public class Movimentacao {
+
+    private Double valor;
+    private String descricao;
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
